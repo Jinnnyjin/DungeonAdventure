7bb0d62db69f8041f589ce8bf9fbb08d7a3da515
Jinnnyjin <jinnnny.jin@gmail.com>
2026-08-27 14:53:02 +0900

    [Add] DungeonGeneration 랜덤워크 구현
    - 시작방에서 4개의 방향으로 랜덤 이동 후 새방 생성
     이미 해당 위치에 만들어진 방이 있다면 연결만 추가
    - 양방향 연결 시 중복 방지 체크

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator
+{
+    // 설정 값
+    private readonly int minRooms;
+    private readonly int maxRooms;
+    private readonly int maxAttempts;
+    private static readonly Vector2Int[] Direction = { Vector2Int.up, Vector2Int.down, Vector2Int.right, Vector2Int.left };
+
+    public DungeonGenerator(int minRooms, int maxRooms, int maxAttempts)
+    {
+        this.minRooms = minRooms;
+        this.maxRooms = maxRooms;
+        this.maxAttempts = maxAttempts;
+    }
+
+    public DungeonGraph Generate()
+    {
+        int nextId = 0;
+        DungeonGraph graph = new DungeonGraph();
+        Dictionary<Vector2Int, Room> occupied = new Dictionary<Vector2Int, Room>();
+        Vector2Int currentPos = Vector2Int.zero;
+
+        int targetCount = Random.Range(minRooms, maxRooms + 1);
+        int attempts = 0;
+
+        // 시작(초기) 방
+        Room startRoom = new Room
+        {
+            Id = nextId,
+            Type = RoomType.Start,
+            GridPos = currentPos,
+            Size = Vector2Int.one,
+            ConnectedRoomIds = new List<int>()
+        };
+
+        nextId++;
+        graph.AddRoom(startRoom);
+        occupied.Add(currentPos, startRoom);
+        
+        // 방 생성 루프
+        while(occupied.Count < targetCount && attempts < maxAttempts)
+        {
+            // 다음 방 위치
+            Vector2Int dir = Direction[Random.Range(0, Direction.Length)];
+            Vector2Int nextPos = currentPos + dir;
+
+            // 현재 방
+            Room currentRoom = occupied[currentPos];
+
+            // 다음 방의 키가 딕셔너리에 등록되어있다면
+            if (occupied.ContainsKey(nextPos))
+            {
+                // 다음 방(이미 존재함)
+                Room existingRoom = occupied[nextPos];
+
+                if (!currentRoom.ConnectedRoomIds.Contains(existingRoom.Id))
+                {
+                    currentRoom.ConnectedRoomIds.Add(existingRoom.Id);
+                    existingRoom.ConnectedRoomIds.Add(currentRoom.Id);
+                }
+            }
+            // 등록되어 있지 않다면
+            else
+            {
+                Room nextRoom = new Room
+                {
+                    Id = nextId,
+                    Type = RoomType.Normal,
+                    GridPos = nextPos,
+                    Size = Vector2Int.one,
+                    ConnectedRoomIds = new List<int>()
+                };
+
+                nextId++;
+                graph.AddRoom(nextRoom);
+                occupied.Add(nextPos, nextRoom);
+
+                currentRoom.ConnectedRoomIds.Add(nextRoom.Id);
+                nextRoom.ConnectedRoomIds.Add(currentRoom.Id);
+            }
+
+            currentPos = nextPos;
+            attempts++;
+        }
+        
+        return graph;
+    }
+
+}
