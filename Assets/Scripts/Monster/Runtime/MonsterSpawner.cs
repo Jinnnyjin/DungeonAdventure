cb56fb68f2a544b3041ea84c7ac32c181a37485b
Jinnnyjin <jinnnny.jin@gmail.com>
2026-09-03 13:01:56 +0900

    [Refactor] MonsterSpawner가 ObjectPoolManager를 쓰도록 마이그레이션
    - 몬스터 전용 Dictionary<GameObject, ObjectPool<Monster>>를 자체 관리하던 방식 제거
    
    - 풀 생성/조회 책임을 공용 ObjectPoolManager로 위임

--- Assets/Scripts/Monster/Runtime/MonsterSpawner.cs
+++ Assets/Scripts/Monster/Runtime/MonsterSpawner.cs
@@ -1,42 +1,21 @@
-using UnityEngine.Pool;
 using UnityEngine;
 using System.Collections.Generic;
 
 public class MonsterSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] monsterPrefabs;
-    private Dictionary<GameObject, ObjectPool<Monster>> monsterPool = new Dictionary<GameObject, ObjectPool<Monster>>();
     [SerializeField] private int minDistance;
 
 
-
-    private void Awake()
-    {
-        foreach (GameObject monsterPrefab in monsterPrefabs)
-        {
-            ObjectPool<Monster> monster = new ObjectPool<Monster>
-                (
-                    createFunc: () => Instantiate(monsterPrefab).GetComponent<Monster>(),
-                    actionOnGet: (monster) => monster.gameObject.SetActive(true),
-                    actionOnRelease: (monster) => monster.gameObject.SetActive(false),
-                    maxSize: 20
-                );
-
-            monsterPool[monsterPrefab] = monster;
-        }
-    }
-
     public Monster SpawnMonster(GameObject monsterPrefab, Vector3 worldPos)
     {
-        Monster monster = monsterPool[monsterPrefab].Get();
+        Monster monster = ObjectPoolManager.Instance.Get<Monster>(monsterPrefab);
         monster.transform.position = worldPos;
         return monster;
     }
 
     public void ReleaseMonster(GameObject monsterPrefab, Monster monster)
     {
-        monsterPool[monsterPrefab].Release(monster);
-
+        ObjectPoolManager.Instance.Release<Monster>(monsterPrefab, monster);
     }
 
     public List<Vector2Int> GetSpawnPositions(RoomTileGrid tileGrid, int count)
