cc622206397c64eec94b2ecb2e27dec90c87dafa
Jinnnyjin <jinnnny.jin@gmail.com>
2026-09-08 23:46:22 +0900

    [Fix] MonoBehaviour 오생성 및 불필요한 import 제거

--- Assets/Scripts/RoomDecorationPlacer.cs
+++ Assets/Scripts/RoomDecorationPlacer.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomDecorationPlacer : MonoBehaviour
+public class RoomDecorationPlacer
 {
     private readonly List<GameObject> decorationPrefabs;
     private readonly float density;
--- Assets/Scripts/TreasureSpawner.cs
+++ Assets/Scripts/TreasureSpawner.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.AdaptivePerformance.Provider.AdaptivePerformanceSubsystemDescriptor;
 
 public class TreasureSpawner : MonoBehaviour
 {
